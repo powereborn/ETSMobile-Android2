dcc9df86544ca972489fe6c16fce82a19639b700
gnut3ll4 <thibaut.tauveron@gmail.com>
2014-10-07 01:17:48 -0400

    Encryption des données de l'utilisateur + correction bugs

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -99,8 +99,8 @@ public final class R {
 		public static final int ic_plusone_small_off_client = 0x7f02003b;
 		public static final int ic_plusone_standard_off_client = 0x7f02003c;
 		public static final int ic_plusone_tall_off_client = 0x7f02003d;
-		public static final int powered_by_google_dark = 0x7f020043;
-		public static final int powered_by_google_light = 0x7f020044;
+		public static final int powered_by_google_dark = 0x7f020044;
+		public static final int powered_by_google_light = 0x7f020045;
 	}
 	public static final class id {
 		public static final int book_now = 0x7f070010;
@@ -123,19 +123,19 @@ public final class R {
 		public static final int selectionDetails = 0x7f07000b;
 		public static final int strict_sandbox = 0x7f070009;
 		public static final int terrain = 0x7f070003;
-		public static final int text1 = 0x7f070098;
+		public static final int text1 = 0x7f07009b;
 		public static final int wrap_content = 0x7f07000d;
 	}
 	public static final class integer {
 		public static final int google_play_services_version = 0x7f090000;
 	}
 	public static final class string {
-		public static final int auth_client_needs_enabling_title = 0x7f0a0080;
-		public static final int auth_client_needs_installation_title = 0x7f0a0081;
-		public static final int auth_client_needs_update_title = 0x7f0a0082;
-		public static final int auth_client_play_services_err_notification_msg = 0x7f0a0083;
-		public static final int auth_client_requested_by_msg = 0x7f0a0084;
-		public static final int auth_client_using_bad_version_title = 0x7f0a0085;
+		public static final int auth_client_needs_enabling_title = 0x7f0a0083;
+		public static final int auth_client_needs_installation_title = 0x7f0a0084;
+		public static final int auth_client_needs_update_title = 0x7f0a0085;
+		public static final int auth_client_play_services_err_notification_msg = 0x7f0a0086;
+		public static final int auth_client_requested_by_msg = 0x7f0a0087;
+		public static final int auth_client_using_bad_version_title = 0x7f0a0088;
 		public static final int common_google_play_services_enable_button = 0x7f0a0015;
 		public static final int common_google_play_services_enable_text = 0x7f0a0016;
 		public static final int common_google_play_services_enable_title = 0x7f0a0017;
@@ -161,8 +161,8 @@ public final class R {
 		public static final int common_google_play_services_update_title = 0x7f0a002b;
 		public static final int common_signin_button_text = 0x7f0a002c;
 		public static final int common_signin_button_text_long = 0x7f0a002d;
-		public static final int pre_release_do_not_ship = 0x7f0a0060;
-		public static final int wallet_buy_button_place_holder = 0x7f0a007e;
+		public static final int pre_release_do_not_ship = 0x7f0a0062;
+		public static final int wallet_buy_button_place_holder = 0x7f0a0081;
 	}
 	public static final class style {
 		public static final int Theme_IAPTheme = 0x7f0b0004;
